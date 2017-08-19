d71f01b76f7fe92f095593b12b5c16d09a723b9b
Casey Curry <casey.de.curry@gmail.com>
2017-08-19 17:46:53 -0500

    fix scripts

--- Dell-XPS-15/CentOS-7/Phase-2-Auto.sh
+++ Dell-XPS-15/CentOS-7/Phase-2-Auto.sh
@@ -8,10 +8,10 @@ sudo yum install -y bzip2 gcc git pkgconfig autoconf automake libtool gperf byac
 sudo yum groupinstall -y "X Window System"
 sudo yum install -y libxcb-devel libXcursor-devel pango-devel pcre-devel perl-Data-Dumper perl-Pod-Parser startup-notification-devel xcb-util-keysyms-devel xcb-util-devel xcb-util-wm-devel yajl-devel check-devel gettext-devel xterm
 sudo yum install -y xorg-x11-xkb-extras xorg-x11-xkb-utils-devel libxkbfile-devel libev-devel
-sudo printf "export XORG_CONFIG=/etc/X11/
+sudo bash -c 'printf "export XORG_CONFIG=/etc/X11/
 export PKG_CONFIG_LIBDIR=/usr/local/lib/pkgconfig/:/usr/lib/pkgconfig/:/usr/lib64/pkgconfig/
 export PKG_CONFIG_PATH=/usr/share/pkgconfig/:/usr/local/share/pkgconfig/
 export ACLOCAL_PATH=/usr/local/share/aclocal/
-export LD_LIBRARY_PATH=/usr/local/lib/" > /etc/profile.d/pclib.sh
+export LD_LIBRARY_PATH=/usr/local/lib/" > /etc/profile.d/pclib.sh'
 
 # reboot the machine so kernel and env vars take place.
