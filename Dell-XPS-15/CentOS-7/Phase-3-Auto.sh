35d537d16b33d11d5b11981be36ab1d61c12f6b4
Casey Curry <casey.de.curry@gmail.com>
2017-08-19 21:05:39 -0500

    fix scripts

--- Dell-XPS-15/CentOS-7/Phase-3-Auto.sh
+++ Dell-XPS-15/CentOS-7/Phase-3-Auto.sh
@@ -6,7 +6,6 @@ git clone https://anongit.freedesktop.org/git/xcb/util-renderutil.git
 git clone https://anongit.freedesktop.org/git/xcb/util-image.git
 git clone https://anongit.freedesktop.org/git/xcb/util-cursor.git
 git clone https://anongit.freedesktop.org/git/xcb/proto.git
-git clone https://anongit.freedesktop.org/git/xcb/libxcb.git
 git clone https://github.com/xkbcommon/libxkbcommon.git
 git clone http://git.savannah.gnu.org/r/confuse.git
 git clone https://github.com/i3/i3.git
@@ -54,14 +53,6 @@ git checkout tags/1.11
 make
 sudo make install
 
-# libxcb
-cd ~/source/libxcb
-git tag -l
-git checkout tags/1.11
-./autogen.sh
-make
-sudo make install
-
 # xkbcommon
 cd ~/source/libxkbcommon
 git tag -l
@@ -91,9 +82,8 @@ sudo yum install -y i3status
 # i3 config
 echo "exec i3" > ~/.xinitrc
 mkdir ~/.i3 && touch ~/.i3/config
-cat /etc/i3/config | sed 's/Mod1/mod/g' > ~/.i3/config
+cat /etc/i3/config | sed 's/Mod1/Mod4/g' > ~/.i3/config
 sed -i '/exec i3-config-wizard/ s/^/#/' ~/.i3/config
-sed -i '1s/^/set $mod Mod4\n\n/' ~/.i3/config
 
 # blacklist nouveau driver
 # http://www.dedoimedo.com/computers/centos-7-nvidia.html
@@ -102,11 +92,5 @@ sudo rpm -e xorg-x11-drivers xorg-x11-drv-nouveau
 sudo bash -c 'echo "blacklist nouveau" > /etc/modprobe.d/blacklist.conf'
 sudo sed -i 's/quiet/quiet rd.driver.blacklist=nouveau/' /etc/default/grub
 sudo grub2-mkconfig -o /boot/efi/EFI/centos/grub.cfg
-
-# download nvidia driver
-# sudo yum install -y kernel-devel kernel-headers gcc make
-# cd ~
-# curl -O http://us.download.nvidia.com/XFree86/Linux-x86_64/384.59/NVIDIA-Linux-x86_64-384.59.run
-# chmod +x NVIDIA-Linux-x86_64-384.59.run
 
 # reboot without nouveau drivers
--- Dell-XPS-15/CentOS-7/Phase-4-Auto.sh
+++ Dell-XPS-15/CentOS-7/Phase-4-Auto.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# disable nvidia
+# disable nvidia gpu
 sudo yum install -y dkms kernel-devel kernel-headers
 cd ~/source
 git clone https://github.com/Bumblebee-Project/bbswitch
@@ -12,58 +12,5 @@ sudo modprobe bbswitch
 sudo bash -c 'echo "bbswitch" > /etc/modules-load.d/bbswitch.conf'
 sudo bash -c 'echo "options bbswitch load_state=0" > /etc/modprobe.d/bbswitch.conf'
 
-
-
-
-
-
-
-# #bumblebee
-# sudo rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-# sudo rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
-# sudo yum install -y bumblebee mesa-libOSMesa
-# sudo printf "[Unit]
-# Description=Bumblebee Daemon
-#
-# [Service]
-# Type=simple
-# CPUSchedulingPolicy=idle
-# ExecStart=/usr/sbin/bumblebeed
-# Restart=always
-# RestartSec=60
-# StandardOutput=kmsg
-#
-# [Install]
-# WantedBy=multi-user.target" > /etc/systemd/system/bumblebeed.service
-# sudo systemctl enable --now bumblebeed
-#
-# # sudo rm /etc/bumblebee/bumblebee.conf
-# sudo printf "[bumblebeed]
-# VirtualDisplay=:8
-# KeepUnusedXServer=false
-# ServerGroup=bumblebee
-# TurnCardOffAtExit=false
-# NoEcoModeOverride=false
-# Driver=nvidia
-# XorgConfDir=/etc/bumblebee/xorg.conf.d
-#
-# [optirun]
-# Bridge=auto
-# VGLTransport=proxy
-# PrimusLibraryPath=/usr/lib/primus:/usr/lib32/primus
-# AllowFallbackToIGC=false
-#
-# [driver-nvidia]
-# KernelDriver=nvidia
-# PMMethod=bbswitch
-# LibraryPath=/usr/lib64/nvidia:/usr/lib64/vdpau:/usr/lib/nvidia:/usr/lib/vdpau
-# XorgModulePath=/usr/lib64/xorg/modules/extensions/nvidia,/usr/lib64/xorg/modules/drivers,/usr/lib64/xorg/modules
-# XorgConfFile=/etc/bumblebee/xorg.conf.nvidia" > /etc/bumblebee/bumblebee.conf
-#
-# # usermod -a -G audio cj
-# # usermod -a -G video cj
-# sudo usermod -a -G bumblebee cj
-#
-#
-#
-# sudo rm -rf ~/source
+# cleanup
+sudo rm -rf ~/source
